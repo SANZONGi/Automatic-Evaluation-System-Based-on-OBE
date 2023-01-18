5f417db93e46a5ded3e0d432557c27ba3b1e7b96
Xjj <2577786020@qq.com>
2023-01-18 23:32:56 +0800

    gap

--- FrontPage/src/views/UserInf.vue
+++ FrontPage/src/views/UserInf.vue
@@ -19,33 +19,7 @@
       <MyHeader/>
       <Layout>
         <Sider hide-trigger :style="{background: '#fff'}">
-          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-            <Submenu name="1">
-              <template #title>
-                <Icon type="ios-navigate"></Icon>
-                Item 1
-              </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
-            </Submenu>
-            <Submenu name="2">
-              <template #title>
-                <Icon type="ios-keypad"></Icon>
-                Item 2
-              </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-              <template #title>
-                <Icon type="ios-analytics"></Icon>
-                Item 3
-              </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
-            </Submenu>
-          </Menu>
+          <MySider/>
         </Sider>
           <Layout :style="{padding: '14px 24px 24px'}">
             <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
@@ -62,7 +36,8 @@
 
 <script>
 import MyHeader from '@/components/MyHeader'
+import MySider from '@/components/MySider'
 export default {
-  components: {MyHeader}
+  components: {MyHeader, MySider}
 }
 </script>
